02ea6e6fd1c46d94b3d05830f4bd2841fadaa942
Minu Mathew <minumpmathews@gmail.com>
2021-09-21 08:25:57 -0500

    Created config file

--- ameriflux_pipeline/main.py
+++ ameriflux_pipeline/main.py
@@ -5,13 +5,14 @@
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
 import os
+import configparser
 import argparse
 import utils.data_util as data_util
 
 from preprocessor import Preprocessor
 
 
-def preprocessing(input_path, output_path, missing_time_threshold, meta_data_path):
+def preprocessing(input_path, output_path, meta_data_path, missing_time_threshold):
     """Create processed dataframe
 
         Args:
@@ -37,23 +38,26 @@ def main(*args):
     Args: None
 
     """
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--input", action="store", default=os.path.join(os.getcwd(), "tests", "data", "FLUXSB_EC.dat.csv"),
                         help="input data path")
     parser.add_argument("--output", action="store", default=os.path.join(os.getcwd(), "tests", "data", "FLUXSB_EC_output.csv"),
                         help="output data path")
-    parser.add_argument("--missingTime", action="store", default=96,
-                        help="Number of 30min timeslot threshold to ask for user confirmation")
     parser.add_argument("--metadata", action="store", default=os.path.join(os.getcwd(), "tests", "data", "FLUXSB_EC.dat.meta.csv"),
                         help="meta data file path")
+    #parser.add_argument("--missingTime", action="store", default=96, help="Number of 30min timeslot threshold to ask for user confirmation")
 
     # parse arguments
 
     args = parser.parse_args()
 
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    missingTime = int(config['MetDataPreprocessor']['missingTime'])
     # start preprocessing data
-    df = preprocessing(args.input, args.output, args.missingTime, args.metadata)
+    df = preprocessing(args.input, args.output, args.metadata, missingTime)
     # write processed df to output path
     data_util.write_data(df, args.output)
 
