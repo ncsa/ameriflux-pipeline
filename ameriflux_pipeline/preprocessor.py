d5e90b92c842c0cd081c8d8a542059f6b865d350
YONG WOOK KIM <ywkim@illinois.edu>
2021-10-21 15:01:55 -0500

    fixed comment

--- ameriflux_pipeline/preprocessor.py
+++ ameriflux_pipeline/preprocessor.py
@@ -355,7 +355,7 @@ class Preprocessor:
     def soil_heat_flux_calculation(shf_mV, shf_cal):
         """Additional calculation for soil heat flux if needed. Step 5 in guide
             shf_Avg=[shf_mV]*[shf_cal]
-
+        """
 
     @staticmethod
     def soil_heat_flux_calculation(shf_mV, shf_cal):
