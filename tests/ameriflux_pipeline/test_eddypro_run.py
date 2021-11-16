fc067d8862b3234b46f421b9a480d49e84b9cb14
YONG WOOK KIM <ywkim@illinois.edu>
2021-11-16 09:01:48 -0600

    removed the real values

--- tests/ameriflux_pipeline/test_eddypro_run.py
+++ tests/ameriflux_pipeline/test_eddypro_run.py
@@ -8,29 +8,18 @@ from ameriflux_pipeline.runeddypro import RunEddypro
 
 
 def test_run_eddypro():
-    # eddypro_bin_loc = "/path/to/LI-COR//EddyPro-7.0.7/bin/"  # directory path to eddypro bin folder
-    # file_name = "/should/be/same/as/tmp/proj_file"
-    # project_title = "Project Title"
-    # project_id = "Project ID"
-    # file_prototype = "file_prototype"  # yyyy-mm-ddTHHMM??_Sorghum-00137.ghg
-    # proj_file = "/path/to/metadata"  # 2021-01-01T000000_Sorghum-00137.metadata
-    # dyn_metadata_file = "/path/to/dyn_metadata"  # Sorghum_2021_dynamic_metadata.csv
-    # out_path = "/path/to/output"
-    # data_path = "/path/to/GHG_files"  # Raw_Jan-Mar_2021_GHG_Files
-    # biom_file = "/path/to/biom_file"  # FLUXSB_EC_JanMar2021_output_eddypro.csv
-    # ex_file = "/path/to/ex_file"  # eddypro_Efarm_Sorghum_Reanalysis_2020_fluxnet_2021-10-26T101711_adv.csv
-
-    eddypro_bin_loc = "C:\\Program Files\\LI-COR\\EddyPro-7.0.7\\bin"
-    file_name = "C:\\Workspace-ameriflux\\ameriflux-pipeline\\ameriflux_pipeline\\templates\\EddyPro_Run_SB_ForNCSA.eddypro"
+    eddypro_bin_loc = "/path/to/LI-COR//EddyPro-7.0.7/bin/"  # directory path to eddypro bin folder
+    file_name = "/should/be/same/as/tmp/proj_file"
     project_title = "Project Title"
     project_id = "Project ID"
-    file_prototype = "yyyy-mm-ddTHHMM??_Sorghum-00137.ghg"
-    proj_file = "C:\\Users\\ywkim\\Documents\\Ameriflux\\Data\\minu_test\\2021-01-01T000000_Sorghum-00137.metadata"
-    dyn_metadata_file = "Sorghum_2021_dynamic_metadata.csv"
-    out_path = "C:\\Users\\ywkim\\Documents\\Ameriflux\\Data\\minu_test\\Test_output"
-    data_path = "C:\\Users\\ywkim\\Documents\\Ameriflux\\Data\\minu_test\\Raw_Jan-Mar_2021_GHG_Files"
-    biom_file = "C:\\Workspace-ameriflux\\ameriflux-pipeline\\tests\\data\\FLUXSB_EC_JanMar2021_output_eddypro.csv"
+    file_prototype = "file_prototype"  # yyyy-mm-ddTHHMM??_Sorghum-00137.ghg
+    proj_file = "/path/to/metadata"  # 2021-01-01T000000_Sorghum-00137.metadata
+    dyn_metadata_file = "/path/to/dyn_metadata"  # Sorghum_2021_dynamic_metadata.csv
+    out_path = "/path/to/output"
+    data_path = "/path/to/GHG_files"  # Raw_Jan-Mar_2021_GHG_Files
+    biom_file = "/path/to/biom_file"  # FLUXSB_EC_JanMar2021_output_eddypro.csv
     ex_file = "/path/to/ex_file"  # eddypro_Efarm_Sorghum_Reanalysis_2020_fluxnet_2021-10-26T101711_adv.csv
+
     RunEddypro.run_eddypro(eddypro_bin_loc=eddypro_bin_loc, file_name=file_name, project_id=project_id,
                            project_title=project_title, file_prototype=file_prototype, proj_file=proj_file,
                            dyn_metadata_file=dyn_metadata_file, out_path=out_path, data_path=data_path,
