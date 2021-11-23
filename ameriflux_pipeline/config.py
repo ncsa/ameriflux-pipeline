ac9459ac22afdc996319b392dd648f5c65a23b89
YONG WOOK KIM <ywkim@illinois.edu>
2021-11-23 15:41:50 -0600

    modified config values

--- ameriflux_pipeline/config.py
+++ ameriflux_pipeline/config.py
@@ -19,25 +19,15 @@ class Config:
     MASTER_MET = os.getenv('MASTER_MET', 'test/data/met_output.csv')  # output data path
 
     # eddypro related parameters
-    EDDYPRO_BIN_LOC = os.getenv('EDDYPRO_BIN_LOC', 'C:/Program Files/LI-COR/EddyPro-7.0.7/bin')
-    EDDYPRO_PROJ_FILE_NAME = os.getenv(
-        'EDDYPRO_PROJ_FILE_NAME', 'ameriflux_pipeline/templates/EddyPro_Run_Template.eddypro')
-    EDDYPRO_PROJ_TITLE = os.getenv(
-        'EDDYPRO_PROJ_TITLE', 'AmeriFlux_Pipeline')
-    EDDYPRO_PROJ_ID = os.getenv(
-        'EDDYPRO_PROJ_ID', 'ameriflux_pipeline')
-    EDDYPRO_FILE_PROTOTYPE = os.getenv(
-        'EDDYPRO_FILE_PROTOTYPE', 'yyyy-mm-ddTHHMM??_Sorghum-00137.ghg')
-    EDDYPRO_PROJ_FILE = os.getenv(
-        'EDDYPRO_PROJ_FILE',
-        'C:/Users/ywkim/Documents/Ameriflux/Data/minu_test/sample_test/2021-01-01T000000_Sorghum-00137.metadata')
-    EDDYPRO_DYN_METADATA = os.getenv(
-        'EDDYPRO_DYN_METADATA',
-        'C:/Users/ywkim/Documents/Ameriflux/Data/minu_test/sample_test/Sorghum_2021_dynamic_metadata.csv')
-    EDDYPRO_OUTPUT_PATH = os.getenv(
-        'EDDYPRO_OUTPUT_PATH', 'C:/Users/ywkim/Documents/Ameriflux/Data/minu_test/sample_test/test_output')
-    EDDYPRO_INPUT_GHG_PATH = os.getenv(
-        'EDDYPRO_INPUT_GHG_PATH', 'C:/Users/ywkim/Documents/Ameriflux/Data/minu_test/sample_test/sample_ghg_files')
+    EDDYPRO_BIN_LOC = os.getenv('EDDYPRO_BIN_LOC', '')
+    EDDYPRO_PROJ_FILE_NAME = os.getenv('EDDYPRO_PROJ_FILE_NAME', 'templates/EddyPro_Run_Template.eddypro')
+    EDDYPRO_PROJ_TITLE = os.getenv('EDDYPRO_PROJ_TITLE', 'AmeriFlux_Pipeline')
+    EDDYPRO_PROJ_ID = os.getenv('EDDYPRO_PROJ_ID', 'ameriflux_pipeline')
+    EDDYPRO_FILE_PROTOTYPE = os.getenv('EDDYPRO_FILE_PROTOTYPE', 'yyyy-mm-ddTHHMM??_Sorghum-00137.ghg')
+    EDDYPRO_PROJ_FILE = os.getenv('EDDYPRO_PROJ_FILE', 'sample_test/2021-01-01T000000_Sorghum-00137.metadata')
+    EDDYPRO_DYN_METADATA = os.getenv('EDDYPRO_DYN_METADATA', 'sample_test/Sorghum_2021_dynamic_metadata.csv')
+    EDDYPRO_OUTPUT_PATH = os.getenv('EDDYPRO_OUTPUT_PATH', 'sample_test/test_output')
+    EDDYPRO_INPUT_GHG_PATH = os.getenv('EDDYPRO_INPUT_GHG_PATH', 'sample_test/sample_ghg_files')
 
     # PyFluxPro related data
     FULL_OUTPUT_PYFLUXPRO = os.getenv('FULL_OUTPUT_PYFLUXPRO', 'tests/data/full_output.csv')
