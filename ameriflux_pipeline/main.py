9da570ec5604554e962cc9fbf9d6e0cf96784064
Minu Mathew <minumpmathews@gmail.com>
2022-03-09 10:56:24 -0600

    added unit formatting check for maeriflux only variables

--- ameriflux_pipeline/main.py
+++ ameriflux_pipeline/main.py
@@ -201,7 +201,6 @@ def pyfluxpro_l1_ameriflux_processing(pyfluxpro_input, l1_mainstem, l1_ameriflux
 if __name__ == '__main__':
     # run eddypro preprocessing and formatting
     eddypro_formatted_met_file, file_meta_data_file = eddypro_preprocessing()
-
     # run eddypro
     run_eddypro(eddypro_formatted_met_file)
 
@@ -226,7 +225,7 @@ if __name__ == '__main__':
         print(cfg.PYFLUXPRO_INPUT_SHEET, "path does not exist")
 
     # run ameriflux formatting of pyfluxpro L1 control file
-    pyfluxpro_l1_ameriflux_processing(cfg.PYFLUXPRO_INPUT_AMERIFLUX, cfg.L1_MAINSTEM, cfg.L1_AMERIFLUX_ONLY,
+    pyfluxpro_l1_ameriflux_processing(cfg.PYFLUXPRO_INPUT_AMERIFLUX, cfg.L1_MAINSTEM_INPUT, cfg.L1_AMERIFLUX_ONLY_INPUT,
                                       cfg.L1_AMERIFLUX_MAINSTEM_KEY, file_meta_data_file, cfg.INPUT_SOIL_KEY,
-                                      cfg.L1_AMERIFLUX_OUTPUT, cfg.L1_AMERIFLUX,
+                                      cfg.L1_AMERIFLUX_RUN_OUTPUT, cfg.L1_AMERIFLUX,
                                       cfg.AMERIFLUX_VARIABLE_USER_CONFIRMATION, cfg.L1_AMERIFLUX_ERRORING_VARIABLES_KEY)
