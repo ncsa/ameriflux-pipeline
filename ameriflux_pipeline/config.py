0dbdbf049c6facf090f1ea9ce01f8e58346edbac
YONG WOOK KIM <ywkim@illinois.edu>
2022-03-11 14:42:58 -0600

    added ameriflux formatting variables

--- ameriflux_pipeline/config.py
+++ ameriflux_pipeline/config.py
@@ -89,16 +89,16 @@ class Config:
                                           '/Users/ameriflux-pipeline/ameriflux_pipeline/data/pyfluxpro/'
                                           'generated/pyfluxpro_input_ameriflux.xlsx')
     L1_MAINSTEM_INPUT = os.getenv('L1_MAINSTEM_INPUT',
-                            '/Users/ameriflux-pipeline/ameriflux_pipeline/data/pyfluxpro/input/L1_mainstem.txt')
-    L1_AMERIFLUX_ONLY_INPUT = os.getenv('L1_AMERIFLUX_ONLY_INPUT',
-                                  '/Users/ameriflux-pipeline/ameriflux_pipeline/data/'
-                                  'pyfluxpro/input/L1_ameriflux_only.txt')
+                                  '/Users/ameriflux-pipeline/ameriflux_pipeline/data/pyfluxpro/input/'
+                                  'L1_mainstem.txt')
+    L1_AMERIFLUX_ONLY_INPUT = os.getenv('L1_AMERIFLUX_ONLY_INPUT', '/Users/ameriflux-pipeline/ameriflux_pipeline/'
+                                                                   'data/pyfluxpro/input/L1_ameriflux_only.txt')
     L1_AMERIFLUX_MAINSTEM_KEY = os.getenv('L1_AMERIFLUX_MAINSTEM_KEY',
                                           '/Users/ameriflux-pipeline/ameriflux_pipeline/data/pyfluxpro/'
                                           'input/Ameriflux-Mainstem-Key.xlsx')
     L1_AMERIFLUX_RUN_OUTPUT = os.getenv('L1_AMERIFLUX_RUN_OUTPUT',
-                                    '/Users/ameriflux-pipeline/ameriflux_pipeline/data/'
-                                    'pyfluxpro/generated/Sorghum_2021_L1.nc')
+                                        '/Users/ameriflux-pipeline/ameriflux_pipeline/data/pyfluxpro/'
+                                        'generated/Sorghum_2021_L1.nc')
     L1_AMERIFLUX = os.getenv('L1_AMERIFLUX',
                              '/Users/ameriflux-pipeline/ameriflux_pipeline/data/pyfluxpro/generated/L1_ameriflux.txt')
 
