87465dce226dfef02463c4c84d5ff66f297c99c9
Minu Mathew <minumpmathews@gmail.com>
2022-07-26 15:19:18 -0500

    removed unwanted import

--- ameriflux_pipeline/pyfluxpro/l2format.py
+++ ameriflux_pipeline/pyfluxpro/l2format.py
@@ -4,7 +4,6 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 
-import pandas as pd
 import os
 import re
 import logging
