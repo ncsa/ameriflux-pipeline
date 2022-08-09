004c6de3ef36ee8d2d644c745c4942d67a9a8016
Minu Mathew <minumpmathews@gmail.com>
2022-08-09 15:45:06 -0500

    added comments and log messages

--- ameriflux_pipeline/master_met/mastermetprocessor.py
+++ ameriflux_pipeline/master_met/mastermetprocessor.py
@@ -129,7 +129,7 @@ class MasterMetProcessor:
             # keep all met data and have NaN for precip values that are missing - left join with met data
             # throw a warning if there are extra timestamps in met data
             if (df.shape[0] > df_precip.shape[0]):
-                # there are more records in met data
+                # there are more records in met data than in precip data
                 log.warning("Extra timestamps in met data. Joining precip with NaN value in extra timestamps")
             # NOTE 8
             df = pd.merge(df, df_precip, on='TIMESTAMP', how='left')
